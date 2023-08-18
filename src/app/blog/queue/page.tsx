6f3736e505b97547b7daea6553de602a81291fcb
Alexander Hortua <alexander.hortua10@gmail.com>
2023-08-18 01:41:00 -0700

    Bringing back Queue

--- src/app/blog/queue/page.tsx
+++ src/app/blog/queue/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-// import { Separator } from "@/components/ui/separator";
-// import { AccountForm } from "./account-form";
-// import Queue from "./view";
+import { Separator } from "@/components/ui/separator";
+import { AccountForm } from "./account-form";
+import Queue from "./view";
 
 export default function SettingsAccountPage() {
   return (
     <>
-      {/* <div className="space-y-6 bg-slate-100 dark:bg-slate-950 p-12 border-black">
+      <div className="space-y-6 bg-slate-100 dark:bg-slate-950 p-12 border-black">
         <div>
           <h3 className="text-lg font-medium">Topic Generator</h3>
           <p className="text-sm text-muted-foreground">
@@ -17,7 +17,7 @@ export default function SettingsAccountPage() {
         <Separator />
         <AccountForm />
       </div>
-      <Queue /> */}
+      <Queue />
     </>
   );
 }
