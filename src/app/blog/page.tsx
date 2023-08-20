7346fa2ca8c68fd61fd837157a15fdbd12c34431
Alexander Hortua <alexander.hortua10@gmail.com>
2023-08-19 23:53:13 -0700

    Displaying Adsense component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,5 @@
 import EntryCard from "@/components/EntryCard";
+import AdsenseClient from "@/components/adSense";
 
 const getData = async () => {
   const data = await fetch(`${process.env.PROJECT_URL}/api/entries`, {
@@ -24,11 +25,13 @@ export default async function Blog() {
           </h2>
 
           <div className="md:grid md:gap-x-6 md:grid-cols-2 lg:grid-cols-3">
-            {data.map((elem: any) => (
+            {data.map((elem: any, idx: number) => (
               <span key={elem._id}>
                 <EntryCard elem={elem} />
+                {idx % 10 == 0 ?? <AdsenseClient />}
               </span>
             ))}
+            <span></span>
           </div>
         </section>
       </div>
---
+++ src/components/adSense.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Adsense } from "@ctrl/react-adsense";
+
+export default function AdsenseClient() {
+  return (
+    <Adsense
+      client="ca-pub-7470419655173226"
+      slot="5753432357"
+      style={{ display: "block" }}
+      layout="-i3-13-1y-6r+rw"
+      format="fluid"
+    />
+  );
+}
