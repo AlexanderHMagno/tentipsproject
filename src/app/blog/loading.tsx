a26b9ef1b543f3563bc37d787db6b8e3fc9580d5
Alexander Hortua <alexander.hortua10@gmail.com>
2023-08-22 18:43:58 -0700

    Adding skeleton to Blogs

--- src/app/blog/loading.tsx
+++ src/app/blog/loading.tsx
@@ -1,11 +1,24 @@
-import { Icons } from "@/components/icons";
+import { Skeleton } from "@/components/ui/skeleton";
 
-function Loading() {
+export default function Blog() {
   return (
-    <div>
-      <Icons.spinner className="animate-spin" />
-    </div>
+    <>
+      <div className="container px-1 mx-auto md:px-6">
+        <section className=" text-center lg:text-left">
+          <h2 className="mb-12 text-center text-3xl font-bold">
+            <Skeleton className="bg-gray-200 h-[250px] w-[250px]"></Skeleton>
+          </h2>
+
+          <div className="md:grid md:gap-x-6 md:grid-cols-2 lg:grid-cols-3">
+            {[1, 2, 3].map((elem: number, idx: number) => (
+              <span key={elem}>
+                <Skeleton className="bg-gray-200 h-[150px] w-[150px]"></Skeleton>
+              </span>
+            ))}
+            <span></span>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
-
-export default Loading;
