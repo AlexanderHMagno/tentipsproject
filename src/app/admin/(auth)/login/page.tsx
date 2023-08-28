a14566b68a6d7dddecdd920ae51f832543376e7d
Alexander Hortua <alexander.hortua10@gmail.com>
2023-08-28 14:37:17 -0700

    Styling login button

--- src/app/admin/(auth)/login/page.tsx
+++ src/app/admin/(auth)/login/page.tsx
@@ -31,12 +31,16 @@ const Login = () => {
         <CardTitle className="text-2xl">Log in to your account</CardTitle>
       </CardHeader>
       <CardContent className="grid gap-4">
-        <div className="grid grid-cols-2 gap-6">
+        <div className="grid  gap-6">
           {/* <Button variant="outline" onClick={() => signIn("facebook")}>
             <Icons.facebook className="mr-2 h-4 w-4" />
             Facebook
           </Button> */}
-          <Button variant="outline" onClick={() => signIn("google")}>
+          <Button
+            variant="outline"
+            className="w-full bg-brand text-white"
+            onClick={() => signIn("google")}
+          >
             <Icons.google className="mr-2 h-4 w-4" />
             Google
           </Button>
