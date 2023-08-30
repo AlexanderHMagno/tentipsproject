20241d59ac8c90e6f5ceb2bfce21fd05c69b2b0c
Alexander Hortua <alexander.hortua10@gmail.com>
2023-08-29 20:13:48 -0700

    Add Banner to all categories

---
+++ src/components/bannerCategories.tsx
@@ -0,0 +1,31 @@
+import { configCache } from "@/lib/api/helpers/connections";
+import { CategoriesType } from "@/models/Categories";
+import Link from "next/link";
+
+export default async function BannerCategories() {
+  const categories = await fetch(
+    `${process.env.PROJECT_URL}/api/categories`,
+    configCache(36000)
+  );
+
+  const data = await categories.json();
+
+  console.log(data);
+
+  return (
+    <ul className="w-[80%] m-auto pb-5 opacity-100 md:opacity-10 ease-in duration-300 hover:opacity-100 flex overflow-hidden hover:overflow-scroll  top-20 text-gray-400">
+      {data.map((cat: CategoriesType, idx: number) => {
+        return (
+          <li className=" ml-10" key={idx}>
+            <Link
+              className=" whitespace-nowrap"
+              href={`/categories/${cat.title}`}
+            >
+              {cat.title}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
