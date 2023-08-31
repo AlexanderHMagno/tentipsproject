cb548b24ed6dc7183848d64a0d440e53be4beb89
Alexander Hortua <alexander.hortua10@gmail.com>
2023-08-30 18:37:29 -0700

    Adding new Submenu Trending

--- src/components/navbar/menu.tsx
+++ src/components/navbar/menu.tsx
@@ -24,6 +24,13 @@ export default function Menu() {
       bigScreen: false,
     },
     { id: 3, title: "Write", path: "/write", auth: true, bigScreen: true },
+    {
+      id: 4,
+      title: "Trending",
+      path: "/trending",
+      auth: true,
+      bigScreen: false,
+    },
     // { id: 4, title: "Compose", path: "/blog/create", auth: authenticated, bigScreen: true },
     // { id: 5, title: "Queue", path: "/blog/queue", auth: authenticated, bigScreen: true },
     // { id: 6, title: "Community  ", path: "/community", auth: authenticated, bigScreen: true },
