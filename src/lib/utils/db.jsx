88673e096d6c168b1c95ea5cbf7cd84fbb29e61e
Alexander Hortua <alexander.hortua10@gmail.com>
2023-10-14 15:42:19 -0700

    Update original CB connection

--- src/lib/utils/db.jsx
+++ src/lib/utils/db.jsx
@@ -1,47 +1,11 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGO;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGO environment variable inside .env.local"
-  );
-}
-
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-
+const connect = async () => {
   try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    throw e;
+    await mongoose.connect(process.env.MONGO);
+  } catch (error) {
+    handleError(error);
   }
-
-  return cached.conn;
-}
+};
 
 export default connect;
